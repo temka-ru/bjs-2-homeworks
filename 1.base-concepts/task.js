764effcdf0787ed93569e95e3d96d26ae05dfa6e
Ekimtsov.av <temkaru84@gmail.com>
2023-09-18 18:05:48 +0700

    Homework for lecture 1. "Basic concepts" Code Listing

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,47 +1,44 @@
-"use strict"; 
+"use strict";
 
 function solveEquation(a, b, c) {
-  let arr = [];
-  let discriminant = b ** 2 - 4 * a * c;
+    let arr = [];
+    let discriminant = b ** 2 - 4 * a * c;
 
-  if (discriminant < 0) {
-    // нет корней
+    if (discriminant < 0) {
+        return arr;
+    } else if (discriminant === 0) {
+        let root = -b / (2 * a);
+        arr.push(root);
+    } else {
+        let rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
+        let rootTwo = (-b - Math.sqrt(discriminant)) / (2 * a);
+        arr.push(rootOne, rootTwo);
+    }
+
     return arr;
-  } else if (discriminant === 0) {
-    // один корень
-    let root = -b / (2 * a);
-    arr.push(root);
-  } else {
-    // два корня
-    let rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
-    let rootTwo = (-b - Math.sqrt(discriminant)) / (2 * a);
-    arr.push(rootOne, rootTwo);
-  }
-
-  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  if (
-    typeof percent !== "number" ||
-    typeof contribution !== "number" ||
-    typeof amount !== "number" ||
-    typeof countMonths !== "number"
-  ) {
-    return false;
-  }
+    if (
+        typeof percent !== "number" ||
+        typeof contribution !== "number" ||
+        typeof amount !== "number" ||
+        typeof countMonths !== "number"
+    ) {
+        return false;
+    }
 
-  percent = percent / 100; // преобразовать проценты в десятичные
-  
-  let principal = amount - contribution; // основная сумма кредита
-  let monthlyInterestRate = percent / 12;
-  let numberOfPayments = countMonths;
+    percent = percent / 100; // преобразовать проценты в десятичные
 
-  let monthlyPayment =
-    principal * (monthlyInterestRate + monthlyInterestRate / 
-    (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1));
+    let principal = amount - contribution; // основная сумма кредита
+    let monthlyInterestRate = percent / 12;
+    let numberOfPayments = countMonths;
 
-  let totalPayment = monthlyPayment * numberOfPayments;
+    let monthlyPayment =
+        principal * (monthlyInterestRate + monthlyInterestRate /
+        (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1));
 
-  return parseFloat(totalPayment.toFixed(2));
+    let totalPayment = monthlyPayment * numberOfPayments;
+
+    return parseFloat(totalPayment.toFixed(2));
 }
