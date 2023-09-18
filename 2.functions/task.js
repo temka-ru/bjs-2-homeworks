fc0efab886d4e4c9f319aff54a90564a7ed3a0a4
Ekimtsov.av <temkaru84@gmail.com>
2023-09-18 17:33:03 +0700

    Homework for lecture 2 "Functions" Task 2 Nozzles converters

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -19,19 +19,59 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
 
+  return arr.reduce((sum, element) => sum + element, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
 
+  let max = Math.max(...arr);
+  let min = Math.min(...arr);
+
+  return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
 
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+    } else {
+      sumOddElement += arr[i];
+    }
+  }
+
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
 
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement++;
+    }
+  }
+
+  return sumEvenElement / countEvenElement;
 }
 
 function makeWork (arrOfArr, func) {
