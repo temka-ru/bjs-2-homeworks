a0d44c64a1366547b452ef01eb19b98810b7ba72
Ekimtsov.av <temkaru84@gmail.com>
2023-09-18 16:06:36 +0700

    task two mortgage interest payment calculator

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -12,14 +12,36 @@ function solveEquation(a, b, c) {
     let root = -b / (2 * a);
     arr.push(root);
   } else {
-    // Tдва корня
-    let root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-    let root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-    arr.push(root1, root2);
+    // два корня
+    let rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let rootTwo = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(rootOne, rootTwo);
   }
 
   return arr;
 }
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  if (
+    typeof percent !== "number" ||
+    typeof contribution !== "number" ||
+    typeof amount !== "number" ||
+    typeof countMonths !== "number"
+  ) {
+    return false;
+  }
+
+  percent = percent / 100; // преобразовать проценты в десятичные
   
+  let principal = amount - contribution; // основная сумма кредита
+  let monthlyInterestRate = percent / 12;
+  let numberOfPayments = countMonths;
+
+  let monthlyPayment =
+    principal * (monthlyInterestRate + monthlyInterestRate / 
+    (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1));
+
+  let totalPayment = monthlyPayment * numberOfPayments;
+
+  return parseFloat(totalPayment.toFixed(2));
 }
