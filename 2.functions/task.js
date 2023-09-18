883b59088619ff3fa9c97b465a39478068617ec9
Ekimtsov.av <temkaru84@gmail.com>
2023-09-18 16:45:11 +0700

    Homework for lecture 2 "Functions"  task 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,19 @@
 function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+
+  const avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
