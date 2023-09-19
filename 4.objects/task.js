6357cc6878cd5272b0705406d42a3c4e4cc9310d
Ekimtsov.av <temkaru84@gmail.com>
2023-09-20 01:24:55 +0700

    Homework for lecture 4 "Objects" Task Student 3

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -9,14 +9,22 @@ Student.prototype.setSubject = function(subjectName) {
     this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+    if (!this.marks) {
+        return;
+    }
+
+    this.marks.push(...marksToAdd);
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+    }
+    const sum = this.marks.reduce((total, mark) => total + mark, 0);
+    return sum / this.marks.length;
 }
 
-Student.prototype.exclude = function (reason) {
+Student.prototype.exclude = function(reason) {
   
 }
