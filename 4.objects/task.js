68f5e10db005b79a216a97090bbe208b040d4d7f
Ekimtsov.av <temkaru84@gmail.com>
2023-09-20 01:30:27 +0700

    Homework for lecture 4 "Objects" Task Student 5

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -21,10 +21,13 @@ Student.prototype.getAverage = function() {
     if (!this.marks || this.marks.length === 0) {
         return 0;
     }
+
     const sum = this.marks.reduce((total, mark) => total + mark, 0);
     return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function(reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
