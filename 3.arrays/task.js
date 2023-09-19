2ba776304a16b2f3fcf8239887542943f3026c46
Ekimtsov.av <temkaru84@gmail.com>
2023-09-20 00:42:06 +0700

    Homework for lecture 3  Task 1-2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false; // If the arrays have different lengths, they are not equal
+    }
+        return arr1.every((value, index) => value === arr2[index]);
 }
+    
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const filteredUsers = users.filter(user => user.gender === gender);
+    const totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+    const averageAge = filteredUsers.length > 0 ? totalAge / filteredUsers.length : 0;
+    
+    return averageAge;
 }
