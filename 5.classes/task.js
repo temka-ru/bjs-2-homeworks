2e34fd88123050b84d8fea6615dda9f0a400e3b5
Ekimtsov.av <temkaru84@gmail.com>
2023-09-20 12:29:46 +0700

    Homework for lecture 5 "Classes" Task 1. Print edition 7

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -28,17 +28,36 @@ class PrintEditionItem {
   
 class Magazine extends PrintEditionItem {
     constructor(name, releaseDate, pagesCount) {
-      super(name, releaseDate, pagesCount);
-      this.type = "magazine";
+        super(name, releaseDate, pagesCount);
+        this.type = "magazine";
     }
-  }
+}
   
-  class Book extends PrintEditionItem {
+class Book extends PrintEditionItem {
     constructor(author, name, releaseDate, pagesCount) {
-      super(name, releaseDate, pagesCount);
-      this.type = "book";
-      this.author = author;
+        super(name, releaseDate, pagesCount);
+        this.type = "book";
+        this.author = author;
     }
 }
 
-
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "novel";
+    }
+}
+  
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "fantastic";
+    }
+}
+  
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "detective";
+    }
+}
