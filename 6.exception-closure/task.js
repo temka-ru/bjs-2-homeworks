67eb0b7347f1896b2e51736ef4f17ecacb2cfff0
Ekimtsov.av <temkaru84@gmail.com>
2023-09-20 19:36:47 +0700

    Homework for lecture 6. "Exception handling and closures"  1

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,16 @@
-﻿
+﻿function parseCount(value) {
+    const parsedValue = Number.parseFloat(value);
+    if (isNaN(parsedValue) || !Number.isFinite(parsedValue)) {
+        throw new Error("Невалидное значение");
+    }
+    return parsedValue;
+  }
+  
+  function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+         return error;
+    }
+  }
+  
