b4ebe100414e23d82fd9acb7647ef78d16c4b136
Ekimtsov.av <temkaru84@gmail.com>
2023-09-20 11:24:40 +0700

    Homework for lecture 5 "Classes" Task 1. Print edition 1-4

---
+++ 5.classes/task.js
@@ -0,0 +1,30 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this._state = 100;
+        this.type = null;
+    }
+  
+    fix() {
+        this.state *= 1.5;
+    }
+  
+    set state(newState) {
+        if (newState < 0) {
+            this._state = 0;
+        } else if (newState > 100) {
+            this._state = 100;
+        } else {
+            this._state = newState;
+        }
+    }
+  
+    get state() {
+        return this._state;
+    }
+}
+  
+
+
