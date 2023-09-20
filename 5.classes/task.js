16111ca9967f6c9589ba4a9e42148cf17dbc6a7b
Ekimtsov.av <temkaru84@gmail.com>
2023-09-20 13:25:43 +0700

    Homework for lecture 5 "Classes" Task 2. Library

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -61,3 +61,36 @@ class DetectiveBook extends Book {
         this.type = "detective";
     }
 }
+
+class Library { 
+    constructor(name) { 
+        this.name = name; 
+        this.books = []; 
+    } 
+     
+    addBook(book) { 
+        if (book.state > 30) { 
+            this.books.push(book); 
+        } 
+    } 
+     
+    findBookBy(type, value) { 
+        for (let book of this.books) { 
+            if (book[type] === value) { 
+                return book; 
+            } 
+        } 
+        return null; 
+    } 
+     
+    giveBookByName(bookName) { 
+        for (let i = 0; i < this.books.length; i++) { 
+            if (this.books[i].name === bookName) { 
+                const book = this.books[i]; 
+                this.books.splice(i, 1); 
+                return book; 
+            } 
+        } 
+        return null; 
+    } 
+} 
